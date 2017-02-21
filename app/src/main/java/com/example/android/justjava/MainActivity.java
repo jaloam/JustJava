d2687e174d0c9a347a2d8fbfbacc243975d4b8c3
jaloam <javierlopezamor@gmail.com>
2017-02-21 23:16:17 +0100

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -44,7 +44,10 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        displayPrice(quantity * 5);
+        int price = quantity * 5;
+        String priceMessage = ("Total: €" + price);
+        priceMessage = priceMessage + "\nGracias.";
+        displayMessage(priceMessage);
     }
     /**
      * This method displays the given quantity value on the screen.
@@ -52,7 +55,17 @@ public class MainActivity extends AppCompatActivity {
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(
                 R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
+        quantityTextView.setText("Total: €" + number);
+    }
+
+
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
     /**
 
